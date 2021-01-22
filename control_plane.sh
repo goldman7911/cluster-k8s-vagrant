806bf05def1903cb9f2581a1df91b5cce9afa1fc
Jankiel Goldman <jankiel.goldman@gmail.com>
2021-01-22 07:01:45 -0300

    add helm

--- control_plane.sh
+++ control_plane.sh
@@ -10,3 +10,11 @@ sudo kubeadm init --apiserver-advertise-address=192.168.33.10 --pod-network-cidr
 #Instalar o CNI Calico
 kubectl create -f https://docs.projectcalico.org/manifests/tigera-operator.yaml
 kubectl create -f /vagrant/custom-resources.yaml
+
+#Instalar Helm
+
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
